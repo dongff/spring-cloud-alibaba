1503d2ca14b44d7123e92e17dc674125eefd535c
dongff <1596624371@qq.com>
2023-02-24 09:51:53 +0800

    springcloud Alibaba

---
+++ alibaba-nacos-discovery-client-common/src/main/java/com/cn/Application.java
@@ -0,0 +1,46 @@
+package com.cn;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author: dongff
+ * @description:
+ * @date: Created in 2023-01-03 10:36
+ * @modified By:
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Slf4j
+    @RestController
+    static class TestController {
+
+        @Autowired
+        LoadBalancerClient loadBalancerClient;
+
+        @GetMapping("/test")
+        public String test() {
+            // 通过spring cloud common中的负载均衡接口选取服务提供节点实现接口调用
+            ServiceInstance serviceInstance = loadBalancerClient.choose("alibaba-nacos-discovery-server");
+            String url = serviceInstance.getUri() + "/hello?name=" + "didi";
+            RestTemplate restTemplate = new RestTemplate();
+            String result = restTemplate.getForObject(url, String.class);
+            return "Invoke : " + url + ", return : " + result;
+        }
+    }
+
+}
